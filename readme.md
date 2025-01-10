33832d714c3b2a408b19f8f54baec0b2fa1d3445
173gvQY8MFaDk9Jt <g@v.q>
2025-01-10 18:00:53 +0800

    first commit

--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-vercel-satori-png-service-font-jetbrains-mono?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-vercel-satori-png-service-font-jetbrains-mono)
 
 Provide JetBrains Mono fonts for the vercel-satori-png-service
+
+---
+
+[JetBrains Mono](https://www.jetbrains.com/lp/mono/)
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,64 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Service } from "koishi";
+import type { Font, FontWeight, FontStyle } from "satori";
+import fs from "node:fs/promises";
+import path from "node:path";
+// noinspection ES6UnusedImports
+import {} from "koishi-plugin-vercel-satori-png-service";
 
-export const name = 'vercel-satori-png-service-font-jetbrains-mono'
+const serviceName = "vercelSatoriPngServiceFontJetbrainsMono";
 
-export interface Config {}
+class VercelSatoriPngServiceFontJetbrainsMono extends Service {
+  private _ctx: Context;
+  constructor(
+    ctx: Context,
+    config: VercelSatoriPngServiceFontJetbrainsMono.Config,
+  ) {
+    super(ctx, serviceName);
+    this._ctx = ctx;
+  }
 
-export const Config: Schema<Config> = Schema.object({})
+  protected async start(): Promise<void> {
+    await this.initFonts();
+  }
+  private async initFonts() {
+    const fonts: Font[] = [];
+    for (let i = 0; i < this.fontNames.length; i++) {
+      const fontName = this.fontNames[i];
+      for (let j = 0; j < 2; j++) {
+        fonts.push({
+          name: "JetBrains Mono",
+          weight: ((i + 1) * 100) as FontWeight,
+          style: ["normal", "italic"][j] as FontStyle,
+          data: await fs.readFile(
+            path.join(
+              __dirname,
+              `../fonts/JetBrainsMonoNL-${j === 0 || fontName !== "Regular" ? fontName : ""}${["", "Italic"][j]}.ttf`,
+            ),
+          ),
+        });
+      }
+    }
+    this._ctx.vercelSatoriPngService.addFont(fonts);
+  }
 
-export function apply(ctx: Context) {
-  // write your plugin here
+  private fontNames: string[] = [
+    "Thin",
+    "ExtraLight",
+    "Light",
+    "Regular",
+    "Medium",
+    "SemiBold",
+    "Bold",
+    "ExtraBold",
+  ];
 }
+
+namespace VercelSatoriPngServiceFontJetbrainsMono {
+  export const inject = ["vercelSatoriPngService"];
+
+  export const usage =
+    '<a target="_blank" href="https://www.jetbrains.com/lp/mono/">JetBrains Mono</a>';
+  export interface Config {}
+  export const Config: Schema<Config> = Schema.object({});
+}
+export default VercelSatoriPngServiceFontJetbrainsMono;
